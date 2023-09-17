270fc00ff897aad1f1c919859407f0abd5f1b192
SamuelOgbonnaeze <nkwachukwusam01@gmail.com>
2023-09-17 03:41:46 +0100

    Daily commit

--- components/StorePage/StoreFooter.jsx
+++ components/StorePage/StoreFooter.jsx
@@ -2,29 +2,30 @@ import React from 'react'
 import Link from 'next/link'
 import { FaTwitter, FaInstagram } from 'react-icons/fa'
 import { AiOutlineFacebook } from 'react-icons/ai'
+import RiTwitterXFill from 'react-icons/ri'
 
 const StoreFooter = () => {
     return (
-        <div className=' w-full h-[420px] -ml-[4px] bg-[#DF3B11] text-white'>
-            <div className='grid grid-cols-2 md:grid-cols-3 gap-6  w-full mx-auto justify-between pt-[65px] px-16'>
+        <div className='w-full  h-[420px]  bg-[#DF3B11] text-white pt-[65px] '>
+            <div className='w-[90%] h-[60%] grid grid-cols-3 gap-6 mx-auto justify-between  '>
                 {/* 1st column */}
-                <div className='flex  flex-col  h-[163px]  font-lato font-normal text-[16px] lg:text-[20px] leading-[20.5px] mx-auto'>
+                <div className='flex  flex-col   font-lato font-normal text-[16px] lg:text-[20px] leading-[20.5px] mx-auto justify-between'>
                     <p className='w-full h-[20px] font-semibold'>Rainbow Fingers</p>
-                    <p className='w-full h-[20px] mt-[27px]'>Online Store</p>
-                    <p className='w-full h-[20px] mt-[27px]'>Online Guitar lessons</p>
-                    <p className='w-full h-[20px] mt-[27px]'>Community (Hub)</p>
+                    <p className='w-full h-[20px] '>Online Store</p>
+                    <p className='w-full h-[20px] '>Online Guitar lessons</p>
+                    <p className='w-full h-[20px] '>Community (Hub)</p>
                 </div>
                 {/* 2nd column */}
-                <div className='flex flex-col  h-[163px] font-lato font-normal text-[16px] lg:text-[20px] leading-[20.5px] mx-auto'>
+                <div className='flex flex-col justify-between  font-lato font-normal text-[16px] lg:text-[20px] leading-[20.5px] mx-auto'>
                     <p className='w-full h-[20px] font-semibold'>Careers</p>
-                    <p className='w-full h-[20px] mt-[17px]'>Privacy policy</p>
-                    <p className='w-full h-[20px] mt-[17px] '>Terms & conditions</p>
-                    <p className='w-full h-[20px] mt-[17px] '>FAQ</p>
+                    <p className='w-full h-[20px] '>Privacy policy</p>
+                    <p className='w-full h-[20px] '>Terms & conditions</p>
+                    <p className='w-full h-[20px] '>FAQ</p>
                 </div>
                 {/* 3rd column */}
-                <div className='flex flex-col col-span-1 h-[163px] font-lato font-normal text-[16px] lg:text-[20px] leading-[20.5px] mt-2 md:mt-0 '>
+                <div className='flex flex-col   font-lato font-normal text-[16px] lg:text-[20px] leading-[20.5px] mt-2 md:mt-0 justify-between '>
                     <p className='w-full h-[20px] font-semibold leading-[13.5px]'>Follow Us</p>
-                    <span className='w-[74px] md:w-[154px] mt-[27px] flex justify-between'>
+                    <span className='w-full md:w-[60%] flex justify-between'>
                         <Link href='/'>
                             <FaTwitter
                                 size={20}
@@ -41,8 +42,8 @@ const StoreFooter = () => {
                             />
                         </Link>
                     </span>
-                    <p className='w-full h-[20px] mt-[27px] '>Email:</p>
-                    <p className='w-full h-[20px] mt-[27px] '>Mobile</p>
+                    <p className='w-full h-[20px]  '>Email:</p>
+                    <p className='w-full h-[20px]  '>Mobile</p>
                 </div>
             </div>
         </div>
